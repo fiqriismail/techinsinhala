1025638be90dfdd9ee72f4b8bfb9f3922601244d
fiqriismail <fiqri.ismail@gmail.com>
2022-08-11 17:30:00 +0530

    LAYOUT: Home page hero section fixed for mobile screen

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,12 +5,12 @@ import HeroImage from "../images/hero.svg"
 const Hero = () => {
   return (
     <section id="hero">
-      <div className="container flex mx-auto items-center px-6 bg-gray-100 justify-between">
-        <h1 class="text-4xl max-w-md  text-left">
+      <div className="container flex flex-col-reverse  mx-auto items-center px-6 bg-gray-100 justify-between md:flex-row">
+        <h1 class="text-2xl  mt-5 max-w-md text-center md:text-4xl md:text-left">
           දන්න සිංහලෙන් ටෙක්නිකල් දේවල් කතා කරමු. ASP.NET, C#, JavaScript,
           Azure... මේ හැම දෙයක්ම!
         </h1>
-        <img src={HeroImage} alt="" className="w-1/2" />
+        <img src={HeroImage} alt="" className="md:w-1/2" />
       </div>
     </section>
   )
