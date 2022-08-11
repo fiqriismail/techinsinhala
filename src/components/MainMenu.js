b741e2ee345aebe22dcbb15a50b55f4ef5bd9472
fiqriismail <fiqri.ismail@gmail.com>
2022-08-11 16:39:06 +0530

    LAYOUT: Menu fixed for mobile phones

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -15,8 +15,8 @@ const MainMenu = () => {
             <div className="">
               <img src={GreenLogo} alt="" />
             </div>
-            <div className="">
-              <h2 className="hidden text-gray-700 text-2xl mx-2 md:flex">
+            <div>
+              <h2 className=" text-gray-700 text-2xl mx-2 md:flex">
                 tech in sinhala
               </h2>
             </div>
@@ -32,26 +32,24 @@ const MainMenu = () => {
               </Link>
             </div>
           </div>
-          <div className="flex space-x-2">
+          <div className="hidden md:flex space-x-2">
             <img src={FABlog} alt="" className="w-12 fill-mainGreen" />
             <img src={FAYouTube} alt="" className="w-12" />
             <img src={FALinkedIn} alt="" className="w-8" />
           </div>
         </div>
-        {/* <div className="flex items-center justify-between">
-          <div className="flex align-middle">
-            <div className="p2">
-              <img src={GreenLogo} alt="" />
-            </div>
-            <h3>tech in sinhala</h3>
-          </div>
-
-          <div className="flex">
-            
-          </div>
-          <div>Social media links</div>
-        </div> */}
       </nav>
+      <div className="md:hidden mx-4 pb-4 space-x-6">
+        <Link to="/" className="hover:text-mainGreen">
+          Home
+        </Link>
+        <Link to="/about" className="hover:text-mainGreen">
+          About Me
+        </Link>
+        <Link to="/courses" className="hover:text-mainGreen">
+          Video Tutorials
+        </Link>
+      </div>
     </section>
   )
 }
